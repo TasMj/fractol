d6e8076b731c457f96620467a7933dc9fcc227a2
Tasnim MEJRI <tmejri@e2r13p8.clusters.42paris.fr>
2022-11-30 16:32:27 +0100

    on continue de dechiffrer

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:08:21 by tmejri            #+#    #+#             */
-/*   Updated: 2022/11/30 15:09:20 by tmejri           ###   ########.fr       */
+/*   Updated: 2022/11/30 16:31:45 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	void	*img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_win += 1;
-	mlx_win -= 1;
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Fractol");
+	img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win +=1;
+	mlx_win -=1;
+	img +=1;
+	img -=1;
+	mlx_pixel_put(mlx, mlx_win, 0, 0, 0xFF0000);
 	
 	mlx_loop(mlx);
-	
-	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:34:45 by tmejri            #+#    #+#             */
-/*   Updated: 2022/11/30 15:05:31 by tmejri           ###   ########.fr       */
+/*   Updated: 2022/11/30 15:43:44 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 #include <stdlib.h>
 #include "./mlx_linux/mlx.h"
 
-
+typedef struct	s_data 
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 #endif
