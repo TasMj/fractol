4599b6d15ed33d3c67267acff0c8a6cdf84abe7f
TasMj <tasnim.mej@hotmail.com>
2022-12-22 11:09:44 +0100

    still small juju

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: tas <tas@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:08:21 by tmejri            #+#    #+#             */
-/*   Updated: 2022/12/22 10:53:25 by tas              ###   ########.fr       */
+/*   Updated: 2022/12/22 11:08:55 by tas              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(void)
 	t_mlx	mlx;
 	t_data	img;
 	// t_fractal	f_m;
-	// t_fractal	f_j;
+	t_fractal	f_j;
 
 	mlx.mlx = mlx_init();
 	mlx.mlx_win = mlx_new_window(mlx.mlx, WIDTH, HEIGHT, "Fractol");
@@ -27,7 +27,7 @@ int	main(void)
 	
 	cadre(&img, 0, 0);
 	// calculate_mandelbrot(&img, f_m);
-	// calculate_julia(&img, f_j);
+	calculate_julia(&img, f_j);
 	// draw_image(img, 0, 0);
 	
 	mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, img.img, 0, 0);
